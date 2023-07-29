f70deda609561992daff9ae8a7ce73efc1a582b6
Rutwik Deshmukh <rutwikdesh28@gmail.com>
2023-07-29 16:55:56 +0530

    stats component updated

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,41 +1,26 @@
 import React from "react";
+import styles from "../style";
+import { stats } from "../constants";
 
 const Stats = () => {
   return (
-    <div className="">
-      <div className="w-full flex flex-row justify-between px-20">
-        <div className="flex flex-row justify-center items-center">
-          <h1 className="text-white text-[38px] font-poppins font-semibold">
-            3800+
-          </h1>
-          <p className="text-gradient px-4 uppercase text-center text-[20px]">
-            User Active
+    <section
+      className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}
+    >
+      {stats.map((stat) => (
+        <div
+          key={stat.id}
+          className="flex flex-1 justify-center items-center flex-row m-3"
+        >
+          <h4 className="text-white font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px]">
+            {stat.value}
+          </h4>
+          <p className="text-gradient font-poppins font-normal xs:text-[21px] text-[15px] xs:leading-[26px] leading-[21px] uppercase ml-3">
+            {stat.title}
           </p>
         </div>
-        <div className="flex items-center">
-          <p className="text-gradient text-center">|</p>
-        </div>
-        <div className="flex flex-row justify-center items-center">
-          <h1 className="text-white text-[38px] font-poppins font-semibold">
-            230+
-          </h1>
-          <p className="text-gradient px-4 uppercase text-center text-[20px]">
-            Trusted by company
-          </p>
-        </div>
-        <div className="flex items-center">
-          <p className="text-gradient text-center">|</p>
-        </div>
-        <div className="flex flex-row justify-center items-center">
-          <h1 className="text-white text-[38px] font-poppins font-semibold">
-            $230M+
-          </h1>
-          <p className="text-gradient px-4 uppercase text-center text-[20px]">
-            Transactions
-          </p>
-        </div>
-      </div>
-    </div>
+      ))}
+    </section>
   );
 };
 
