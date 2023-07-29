032e79d8b7d0cbd3aa9e9878b0154c2a061e3665
Rutwik Deshmukh <rutwikdesh28@gmail.com>
2023-07-29 16:04:26 +0530

    hero component images alignment fixed

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
         </div>
       </div>
 
-      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
           <Hero />
         </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ import GetStarted from "./GetStarted";
 const Hero = () => (
   <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
     <div
-      className={`flex-1 ${styles.flexStart} flex-col md:px-0 sm:px-16 px-6`}
+      className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
     >
       <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
         <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
@@ -38,9 +38,7 @@ const Hero = () => (
       </p>
     </div>
 
-    <div
-      className={`md:my-[0px] my-[20px] flex-1 flex relative ${styles.flexCenter} `}
-    >
+    <div className={`md:my-0 my-10 flex-1 flex relative ${styles.flexCenter} `}>
       <img
         src={robot}
         alt="robot.png"
